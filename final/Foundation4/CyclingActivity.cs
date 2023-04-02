82d0a3a1ef5ef01d414e924d5aa5b16589b3c6af
Keaton Davis <Keaton32davis@gmail.com>
2023-04-01 21:41:57 -0600

    Final

---
+++ final/Foundation4/CyclingActivity.cs
@@ -0,0 +1,12 @@
+﻿namespace Foundation4;
+class CyclingActivity : Activity
+{
+    private int speed;
+    public CyclingActivity(DateTime date, int length, int speed) : base(date, length)
+    {
+        this.speed = speed;
+    }
+    public override double GetDistance() { return speed / 60.0 * Length; }
+    public override double GetSpeed() { return speed; }
+    public override double GetPace() { return 60.0 / speed * GetDistance(); }
+}
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,21 @@
-using System;
-
+namespace Foundation4;
 class Program
-{
-    static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        static void Main(string[] args)
+        {
+            RunningActivity running = new RunningActivity(DateTime.Parse("09/05/2023"), 10, 4.5);
+            CyclingActivity cycling = new CyclingActivity(DateTime.Parse("01/05/2023"), 90, 30);
+            SwimmingActivity swimming = new SwimmingActivity(DateTime.Parse("10/05/2022"), 120, 20);
+
+            List<Activity> activities = new List<Activity>();
+            activities.Add(running);
+            activities.Add(cycling);
+            activities.Add(swimming);
+
+            foreach (Activity activity in activities)
+            {
+                Console.WriteLine(activity.GetSummary());
+            }
+
+        }
     }
-}
